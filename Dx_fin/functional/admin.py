fc9f116a8ccca40847bd77bf3549ead0528f265d
Vardan88 <76653394+Vardan88@users.noreply.github.com>
2023-06-26 01:49:08 +0400

    Update admin.py

--- Dx_fin/functional/admin.py
+++ Dx_fin/functional/admin.py
@@ -9,7 +9,7 @@ class FunctionalAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
-    list_editable = ('is_published',)
+ 
     
 
 
